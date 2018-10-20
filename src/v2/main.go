9148b5a8fa1e9bd8b7643a849cc961b67254d728
Sandeep <Sandeep.Mendiratta@target.com>
2018-10-20 12:18:17 -0500

    initial stuff1

---
+++ src/v2/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+func main() {
+var router = mux.NewRouter()
+router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
+router.HandleFunc("/message", handleQryMessage).Methods("GET")
+router.HandleFunc("/m/{msg}", handleUrlMessage).Methods("GET")
+router.HandleFunc("/", Index)
+router.HandleFunc("/todos/{todoId}", TodoShow)
+
+
+fmt.Println("Running server!")
+log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Still alive!")
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+//curl http://localhost:3000/message\?msg\=hello
+func handleQryMessage(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	message := vars.Get("msg")
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+//curl http://localhost:3000/m/helloworld
+
+func handleUrlMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	message := vars["msg"]
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+//curl http://localhost:3000/todos/4
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintf(w, "Todo show: %s\n", todoId)
+}
